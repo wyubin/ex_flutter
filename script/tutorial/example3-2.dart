13ba6b16ed408a24211f53e1ba82b742a38af206
wyubin <yubin0611@gmail.com>
2021-07-10 09:15:13 +0000

    complete ex3-2

--- script/tutorial/example3-2.dart
+++ script/tutorial/example3-2.dart
@@ -1,3 +1,58 @@
-void main() {}
+void main() {
+  Cat f = Cat('花花');
+  f.speak();
+  PetLikeCat h = PetLikeCat('小華');
+  h.speak();
+  SmallPet m = SmallPet();
+  m.speak();
+  print(m.name);
+  var noHomeCat = NoHome();
+  print(noHomeCat.name);
+}
 
-class People {}
+/// 基本寵物 class
+abstract class Pet {
+  String? name;
+  int? age;
+  String _call = 'hello~';
+
+  Pet(this.name, this.age);
+  void speak() {
+    print(_call);
+  }
+}
+
+/// 貓模型大便
+class Cat extends Pet {
+  String _call = 'mao~';
+  Cat(String name) : super(name, null);
+  void speak() {
+    print(_call * 2);
+  }
+}
+
+// mixin
+/// mao 能力
+mixin Mao {
+  void speak() {
+    print('mao~mao~mao~');
+  }
+}
+
+class PetLikeCat extends Pet with Mao {
+  PetLikeCat(String name) : super(name, null);
+}
+
+class SmallPet extends PetLikeCat {
+  SmallPet() : super('小貓');
+}
+
+// implements
+class NoHome implements Cat {
+  String? name = 'noName';
+  int? age;
+  String _call = 'maoooo~';
+  void speak() {
+    print(_call * 3);
+  }
+}
--- wiki/ex3.md
+++ wiki/ex3.md
@@ -5,6 +5,8 @@ Class及繼承
 [link](https://ithelp.ithome.com.tw/articles/10215207)
 [Day14：類別與建構式](https://ithelp.ithome.com.tw/articles/10236606)
 [Day 15：方法(Method)、getter 以及 setter](https://ithelp.ithome.com.tw/articles/10237210)
+[Day 16：抽象類別 (Abstract class)](https://ithelp.ithome.com.tw/articles/10237935)
+[Day18：Ｍixin](https://ithelp.ithome.com.tw/articles/10239174)
 
 # class
 - 通常先宣告property
@@ -20,3 +22,21 @@ Class及繼承
 - 通常一個 property 需要同時有 get/set
 - get/set 可以視作以 function 來定義 property 應該被怎設定，通常作為與其他已宣告屬性的轉換
 
+# 抽象類別 (Abstract class)
+- 無法被實例化，只能被其他類別繼承
+- 最好有 non-named constructor 讓子 class 能夠塞 property
+- 註解是用 "///"，在 function or class 的前面
+
+# extends 繼承
+- 僅僅單一繼承
+- 可以複寫 super 的 property 與 method
+
+# mixin
+- 相對於extends，mixin 有點像是後天繼承，但也會把相關屬性留給子 class
+- 偏向於單一(較少)屬性的繼承方式
+- 可以多個 mixin 加在同一 class
+
+# implements
+設定物件須實做相關的屬性方法
+- 算是規範物件實做的屬性與方法
+- 可能在實做上面會不常使用
